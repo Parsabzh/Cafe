22091ec683b885131be0e5a8a0a485d2225bb232
Parsabzh <parsabzh@gmail.com>
2020-02-26 17:33:28 +0330

    Add files via upload

---
+++ Cafe7.Model/Invoice/InvoiceItemModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cafe7.Infrastructure.Domain;
+using Cafe7.Model.Customer;
+using Cafe7.Model.Product;
+
+namespace Cafe7.Model.Invoice
+{
+    [Table("InvoiceItem",Schema = "Store")]
+   public class InvoiceItemModel:ModelBase<int>
+    {
+      
+       
+        
+       
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        public int Qty { get; set; }
+
+
+        public string PName { get; set; }
+
+        public CustomerModel Customer { get; set; }
+        public  InvoiceModel Invoice { get; set; }
+
+
+    }
+}
---
+++ Cafe7.Model/Invoice/InvoiceModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cafe7.Infrastructure.Domain;
+using Cafe7.Infrastructure.ValidationResource;
+using Cafe7.Model.Customer;
+using Cafe7.Model.People;
+
+namespace Cafe7.Model.Invoice
+{
+    [Table("Invoice",Schema ="Store" )]
+  public class InvoiceModel:ModelBase<int>
+    {
+        
+        public string Date { get; set; }
+
+       
+        
+
+        [RegularExpression(@"^[0-9]{1,10}$", ErrorMessageResourceType = typeof(ValidationResource), ErrorMessageResourceName = "RegularExpression")]
+
+        public int TableNumber { get; set; }
+
+        public string Total { get; set; }
+
+
+        public int InvoiceNumber { get; set; }
+
+    }
+}
